5acfe0e93246b9d77d04fdc70318aa89fabb5842
Martin Zalazar <mzalazar.linux@gmail.com>
2019-08-09 13:14:21 -0300

    Some changes (EventEmitter, index existence verification, etc.)

--- sentinel.js
+++ sentinel.js
@@ -1,6 +1,7 @@
 'use strict'
 const { spawn, fork } = require('child_process')
 require('better-log/install')
+const EventEmitter = require('events')
 const path = require('path')
 const colors = require('colors')
 const numCores = require('os').cpus().length
@@ -34,10 +35,11 @@ const process_options = {
 	stdio: 'inherit'
 }
 
-class Indexer {
+class Indexer extends EventEmitter {
 
 	constructor(filename) {
-		this.minFileSize = 100
+		super()
+		this.minFileSize = 100 // In megabytes
 		this.chunkSize = 10000000 // MUST BE AN EVEN NUMBER
 		this.filesize = null
 		this.filename = filename
@@ -50,11 +52,11 @@ class Indexer {
 
 		process.on('uncaughtException', FATAL)
 
-		this.start()
+		this.startIndexing()
 			.catch(FATAL)
 	}
 
-	async start() {
+	async startIndexing() {
 		// VALIDATE PARAMETER
 		if (typeof this.filename !== 'string' || this.filename.length === 0) {
 			throw new Error('Filename is required!')
@@ -64,41 +66,61 @@ class Indexer {
 		// VALIDATE FILE EXISTENCE (getting filesize)
 		this._getFileSize()
 
-		// VALIDATE FILE SIZE
-		/*		if (this.filesize < this.minFileSize * mb) { ** LO COMENTO POR AHORA
-					throw new Error(`Cannot index a file less than ${this.minFileSize}Mb`)
-				}*/
+		const indexExists = this.checkIndexExistence()
+		if (indexExists) {
+			LOG('Index already exists, no need to reindex.')
+			this.emit('indexed')
+		} else {
+			// VALIDATE FILE SIZE
+			/*		if (this.filesize < this.minFileSize * mb) { ** LO COMENTO POR AHORA
+						throw new Error(`Cannot index a file less than ${this.minFileSize}Mb`)
+					}*/
 
-		// FIX CARRIAGE RETURN (if option is set)
-		if (this.fixCRLF) {
-			await this._fixCRLF()
+			// FIX CARRIAGE RETURN (if option is set)
+			if (this.fixCRLF) {
+				await this._fixCRLF()
+			}
+
+			this._divideCpuLoad()
+
+			// CREATE TEMP DIRECTORY (FOR INDEXED PARTS)
+			this._createTempDir()
+
+			// LAUNCH WORKERS
+			this.launchMultithread()
+			/*		} else {
+						// LAUNCH A SINGLE THREAD
+						this.launchSinglethreaded()
+					}*/
 		}
+	}
 
-		this._divideCpuLoad()
-
-		// CREATE TEMP DIRECTORY (FOR INDEXED PARTS)
-		this._createTempDir()
-
-		// LAUNCH WORKERS
-		this.launchMultithread()
-		/*		} else {
-					// LAUNCH A SINGLE THREAD
-					this.launchSinglethreaded()
-				}*/
+	checkIndexExistence() {
+		try {
+			const size = fs.statSync(`${this.filename}.index`).size
+			// We can do an exaustive index testing here.
+			return size > 0
+		} catch (err) {
+			if (err.code && err.code === 'ENOENT') {
+				return false
+			} else {
+				FATAL(err, 'Error checking index existence.')
+			}
+		}
 	}
 
 	launchMultithread() {
-		console.log('JOBS:'.yellow)
-		console.log(this.chunks)
-		console.log('Master process is running with pid:', process.pid)
-		console.log(`CORES: ${numCores}`)
+		LOG('JOBS:'.yellow)
+		LOG(this.chunks)
+		LOG('Master process is running with pid:', process.pid)
+		LOG(`CORES: ${numCores}`)
 		for (let i = 0; i < numCores; i++) {
 			const thread = fork('./indexer_multithread.js', [this.filename, this.tempDir], process_options)
 			this.threads++
-			console.log(`Launched thread id: ${thread.pid}`)
+			LOG(`Launched thread id: ${thread.pid}`)
 			// ERROR HANDLER
 			thread.on('error', (err) => {
-				console.log(`Thread ${thread.id} throws an error: ${err}`)
+				LOG(`Thread ${thread.id} throws an error: ${err}`)
 				process.exit(1)
 			})
 
@@ -114,22 +136,21 @@ class Indexer {
 							let chunk = this.chunks.shift()
 							thread.send({ type: 'CHUNK', data: chunk })
 						} else {
-							console.log('No more chunks available, sending SIGTERM to thread.'.bgRed.black)
+							LOG('No more chunks available, sending SIGTERM to thread.'.bgRed.black)
 							thread.kill('SIGTERM')
 						}
 						break;
 					default:
-						console.log('default')
-						console.log(msg)
+						LOG(msg)
 				}
 			})
 
 			thread.on('exit', () => {
-				console.log('Thread exit!')
+				LOG('Thread exit!')
 				// If NOT flushing index right now... then do it!
 				this.threads--
 				if (this.threads === 0) {
-					console.log('LAST THREAD HAS FINISHED!'.rainbow)
+					LOG('LAST THREAD HAS FINISHED!'.rainbow)
 					this.mergeIndexes()
 				}
 			})
@@ -137,7 +158,7 @@ class Indexer {
 	}
 
 	mergeIndexes() {
-		console.log('STARTING MERGE'.bgRed.black)
+		LOG('STARTING MERGE'.bgRed.black)
 		let masterIndex = this.createMasterIndex()
 		const self = this;
 		// options is optional
@@ -145,12 +166,12 @@ class Indexer {
 		let master_offset = 0
 		files.forEach((e, i) => {
 			//			let lastOne = (i === files.length - 1 ? true : false)
-			console.log(`Processing ${e}`)
+			LOG(`Processing ${e}`)
 			let size = fs.statSync(e).size
 			let indexPart = fs.openSync(e, 'r')
 			let buffer = Buffer.alloc(size)
 			let bytesReaded = fs.readSync(indexPart, buffer, 0, size, 0) // READ ENTIRE FILE
-			console.log(`bytesReaded: ${bytesReaded}`)
+			LOG(`bytesReaded: ${bytesReaded}`)
 			// Write each offset (readed from file) and add master_offset value to every offset found.
 			let newData = Buffer.alloc(size)
 			let newValue
@@ -167,7 +188,7 @@ class Indexer {
 			}
 			// Flush data to index file!
 			let bytesWritten = fs.writeSync(masterIndex, newData, 0)
-			console.log(`Written ${bytesWritten} bytes.`)
+			LOG(`Written ${bytesWritten} bytes.`)
 			// Here... we need to PARSE last "offset" detected (\n) and subtract that offset
 			master_offset += this.chunkSize
 			// Close file
@@ -177,9 +198,8 @@ class Indexer {
 		fs.closeSync(masterIndex)
 
 		// REMOVE DIRECTORY AND FILES
-		setTimeout(() => {
-			self.deleteFolderRecursive(self.tempDir)
-		}, 1000)
+		self.deleteFolderRecursive(self.tempDir)
+		this.emit('indexed')
 	}
 
 	createMasterIndex() {
@@ -189,12 +209,12 @@ class Indexer {
 	}
 
 	deleteFolderRecursive(path) {
-		console.log(`deleting ${this.tempDir}`)
+		LOG(`deleting ${this.tempDir}`)
 		if (fs.existsSync(path)) {
 			fs.readdirSync(path).forEach(function (file, index) {
 				var curPath = `${path}/${file}`
-				console.log(`To be deleted: ${path}/${file}`)
-				process.exit()
+				LOG(`To be deleted: ${path}/${file}`)
+				//				process.exit()
 				if (fs.lstatSync(curPath).isDirectory()) { // recurse
 					deleteFolderRecursive(curPath)
 				} else { // delete file
@@ -206,12 +226,12 @@ class Indexer {
 	}
 
 	launchSinglethreaded() {
-		console.log('launchSinglethreaded.')
+		LOG('launchSinglethreaded.')
 		const thread = fork('./indexer_singlethread.js', [this.filename])
 
 		// CHECK IF INDEXER FINISH
 		thread.on('exit', (code) => {
-			console.log(`child process exited with code ${code}`)
+			LOG(`child process exited with code ${code}`)
 			process.exit() // EXIT MAIN PROCESS
 		})
 	}
@@ -219,7 +239,7 @@ class Indexer {
 
 	_getFileSize() {
 		let data
-		console.log(`CHECKING: ${this.filename}`)
+		LOG(`CHECKING: ${this.filename}`)
 		data = fs.statSync(this.filename)
 		this.filesize = data.size
 		this.isBigFile = this.filesize >= (this.minFileSize * mb)
@@ -233,11 +253,10 @@ class Indexer {
 		let chunksCount = Math.ceil(filesize / this.chunkSize)
 		let remainderBytes = this.filesize - (this.chunkSize * (chunksCount - 1))
 		let currentOffset = 0
-		console.log(`chunksCount: ${chunksCount}`)
+		LOG(`chunksCount: ${chunksCount}`)
 		for (let i = 1; i <= chunksCount; i++) {
 			this.chunks.push({ start: currentOffset, end: (i === chunksCount ? currentOffset + remainderBytes : currentOffset + this.chunkSize), part: i })
 			currentOffset += this.chunkSize // Increment by chunkSize
-			//			remainderBytes -= this.chunkSize // Decrement by chunkSize
 		}
 	}
 
@@ -250,7 +269,7 @@ class Indexer {
 
 	_fixCRLF() {
 		return new Promise((resolve, reject) => {
-			console.log('Fixing CRLF...')
+			LOG('Fixing CRLF...')
 			setTimeout(resolve, 100)
 			/*
 						const readableStream = fs.createReadStream(this.filename)
@@ -283,16 +302,22 @@ class Indexer {
 			*/
 		})
 	}
-	readLine(lineNum) {
-		// *** read from coordinates until NEXT CARRIAGE RETURN ***
-		let indexFileName = '50m.txt.index'
-		let mainFileName = '50m.txt'
+	async readLine(lineNum) {
+		let indexFileName = `${this.filename}.index`
+		let mainFileName = this.filename
+		// CHECK INDEX EXISTENCE
+		const indexExists = this.checkIndexExistence()
+		if (indexExists) {
+			LOG('Index already exists, no need to reindex.')
+		} else {
+			await startIndexing()
+		}
 		let indexHandle = fs.openSync(indexFileName)
 		let fileHandle = fs.openSync(mainFileName)
 		let maxLine = (fs.statSync(indexFileName).size / 5) - 1
+		LOG(`lineNum: ${lineNum}`)
+		LOG(`maxLine: ${maxLine}`)
 		// compare if maxLine is reached.
-		console.log(`lineNum: ${lineNum}`)
-		console.log(`maxLine: ${maxLine}`)
 		if (lineNum <= maxLine) {
 			let bufferFromIndex = Buffer.alloc(10)
 			fs.readSync(indexHandle, bufferFromIndex, 0, 10, (lineNum - 1) * 5)
@@ -304,40 +329,46 @@ class Indexer {
 			let length = fileOffsetTo - fileOffsetFrom
 			let bufferFromFile = Buffer.alloc(length)
 			fs.readSync(fileHandle, bufferFromFile, 0, length, fileOffsetFrom)
-			console.log(`------>${bufferFromFile.toString()}<------`)
+			//			LOG(`------>${bufferFromFile.toString()}<------`)
+			return bufferFromFile.toString()
 		} else {
-			console.log('Max line was reached, can\'t read after EOF!')
-			process.exit(1)
+			FATAL('Max line was reached, can\'t read after EOF!')
 		}
 	}
 
-	readLines(fromLine, toLine) {
-		let indexFileName = '50m.txt.index'
-		let mainFileName = '50m.txt'
+	async readLines(fromLine, toLine) {
+		let indexFileName = `${this.filename}.index`
+		let mainFileName = this.filename
+		// CHECK INDEX EXISTENCE
+		const indexExists = this.checkIndexExistence()
+		if (indexExists) {
+			LOG('Index already exists, no need to reindex.')
+		} else {
+			await this.startIndexing()
+		}
 		let indexHandle = fs.openSync(indexFileName)
 		let fileHandle = fs.openSync(mainFileName)
 		let maxLine = (fs.statSync(indexFileName).size / 5) - 1
 		// Some validation
 		if (fromLine >= toLine || toLine > maxLine) {
-			console.log('An error has occurred, i can\'t give you what you want... i think some numbers are misplaced.')
-			process.exit(1)
+			FATAL('An error has occurred, i can\'t give you what you want... i think some numbers are misplaced.')
 		}
 		let bufferFromIndex = Buffer.alloc(5)
 		let bufferToIndex = Buffer.alloc(5)
 		fs.readSync(indexHandle, bufferFromIndex, 0, 5, (fromLine - 1) * 5)
 		fs.readSync(indexHandle, bufferToIndex, 0, 5, (toLine) * 5)
 		let fileOffsetFrom = bufferFromIndex.readUIntBE(0, 5)
-		console.log(`fileOffsetFrom: ${fileOffsetFrom}`)
+		LOG(`fileOffsetFrom: ${fileOffsetFrom}`)
 		let fileOffsetTo = bufferToIndex.readUIntBE(0, 5)
 		if (!SHOW_LAST_LF) {
 			fileOffsetTo-- // FIX (doesn't print last LF)
 		}
-		console.log(`fileOffsetTo: ${fileOffsetTo}`)
+		LOG(`fileOffsetTo: ${fileOffsetTo}`)
 		let length = fileOffsetTo - fileOffsetFrom
 		let bufferFromFile = Buffer.alloc(length)
 		fs.readSync(fileHandle, bufferFromFile, 0, length, fileOffsetFrom)
-		//    console.log(`------>${bufferFromFile.toString().replace(/\s/g, '_')}<------`)
-		console.log(`------>${bufferFromFile.toString()}<------`)
+		//		LOG(`------>${bufferFromFile.toString()}<------`)
+		return bufferFromFile.toString()
 	}
 
 }
--- test.js
+++ test.js
@@ -1,4 +1,20 @@
-const i = require('./sentinel')
-let f = new i('./50m.txt');
+const { spawn, fork } = require('child_process')
+const indexer = require('./sentinel')
+const fs = require('fs')
+const path = require('path')
+const process_options = {
+  stdio: 'inherit'
+}
+
+const filename = './testfile.txt'
+//let f = new i('./50m.txt');
 //let f = new i('./testfile2.txt');
 
+// Indexado se inicia al querer leer alguna/s linea/s
+let i = new indexer(filename)
+i.on('indexed', () => {
+  i.readLines(900000, 900010)
+    .then((lines) => {
+      console.log('Lineas leídas:', lines)
+    })
+})
